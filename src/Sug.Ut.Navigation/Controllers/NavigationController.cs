f5e611e81095e443c70fe111b53e0b573f412c0c
Volodymyr HIl <vhi@cabana.dk>
2016-10-16 20:13:08 +0200

    design sitecore items

---
+++ src/Sug.Ut.Navigation/Controllers/NavigationController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Sug.Ut.Navigation.Controllers
+{
+	public class NavigationController : Controller
+	{
+		public ActionResult PrimaryNavigation()
+		{
+			return this.View();
+		}
+	}
+}
