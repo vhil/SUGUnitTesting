8ce4a9671a1375e5f34214d694df90d778c3c8a6
Volodymyr HIl <vhi@cabana.dk>
2016-11-15 12:16:56 +0100

    fakedb demo

---
+++ src/Sug.Ut.SitecoreExtensions.Tests.FakeDb/FakeDbDemo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Sitecore.Data;
+using FluentAssertions;
+using NUnit.Framework;
+using Sitecore.FakeDb;
+
+namespace Sug.Ut.SitecoreExtensions.Tests.FakeDb
+{
+	[TestFixture]
+	public class FakeDbDemo
+	{
+		[Test]
+		public void FakeDbDemo_ShowsHowNiceIsFakeDb()
+		{
+			
+		}
+	}
+}
